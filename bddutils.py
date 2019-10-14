6386c3ed92786aee0b7191a416f7095739c490cb
Xieyang Xu <ashlippers@Xieyangs-MacBook-Pro.local>
2019-10-14 15:30:53 -0700

    initial commit YAML loading converting ACLs and forwarding tables to predicates

---
+++ bddutils.py
@@ -0,0 +1,106 @@
+import ipaddress
+from pyeda.inter import *
+
+# ipn = ipaddress.ip_network('1.0.0.0/24')
+# addr = ipn.network_address.packed
+# pref = ipn.prefixlen
+# print(ipn.network_address.packed)
+# print(ipn.prefixlen)
+
+# x = bddvars('x', 32)
+# f = expr2bdd(x[0] & ~x[1])
+# print(bdd2expr(f))
+
+# f1 = expr2bdd(~x[0] & ~x[1])
+# f = f | f1
+# f = expr2bdd(x[0] | ~ x[0])
+# print(bdd2expr(f))
+
+
+def ipp2bdd(ipprefix='0.0.0.0/0', namespace='dip'): # convert a ip prefix to BDD
+    ipn = ipaddress.ip_network(ipprefix)
+    addr_int = int(ipn.network_address)
+    addr = [addr_int >> i & 1 for i in range(31,-1,-1)] # 32-bit vector
+    preflen = ipn.prefixlen # prefix length
+    
+    x = bddvars(namespace, 32)
+    f = bddones(1)[0] # init with trivial value f=true
+
+    for i in range(preflen):
+        if addr[i]:
+            f = f & x[i]
+        else:
+            f = f & ~x[i]
+    
+    return f
+
+def less2bdd(v, bitlen, namespace): # BDD version of f(x): x<v?
+    f = bddzeros(1)[0] # init with false
+    v_bin = [v >> i & 1 for i in range(bitlen-1, -1, -1)] # bit vector
+
+    x = bddvars(namespace, bitlen)
+    for i in range(bitlen):
+        if v_bin[i]:
+            tmp = bddones(1)[0]
+            for j in range(i): # 0 ~ i-1
+                if v_bin[j]:
+                    tmp = tmp & x[j]
+                else:
+                    tmp = tmp & ~x[j]
+            tmp = tmp & ~x[i] # i
+            f = f | tmp
+    return f
+
+def equal2bdd(v, bitlen, namespace): # BDD version of f(x): x==v?
+    f = bddones(1)[0] # init with true
+    v_bin = [v >> i & 1 for i in range(bitlen-1, -1, -1)] # bit vector
+    
+    x = bddvars(namespace, bitlen)
+    for i in range(bitlen):
+        if v_bin[i]:
+            f = f & x[i]
+        else:
+            f = f & ~x[i]
+    return f
+
+def range2bdd(vrange, bitlen, namespace): # BDD version of f(x): x in vrange?
+    tmp = vrange.split('-')
+    vstart = int(tmp[0])
+    vend = int(tmp[1])
+    f_equal_to_end = equal2bdd(vend, 8, namespace)
+    f_less_than_end = less2bdd(vend, 8, namespace)
+    f_less_than_start = less2bdd(vstart, 8, namespace)
+    f = f_equal_to_end | (f_less_than_end & ~f_less_than_start)
+    return f
+
+
+def aclr2bdd(acl_rule): # convert an ACL rule to BDD
+    allowed = bddzeros(1)[0]
+    denied = bddzeros(1)[0]
+
+    # protocol
+    f_protocol = range2bdd(acl_rule['Protocol'], 8, 'pro')
+    
+    # dst ip
+    f_dstip = ipp2bdd(acl_rule['DstIp'], 'dip')
+
+    # src ip
+    f_srcip = ipp2bdd(acl_rule['SrcIp'], 'sip')
+
+    # dst port
+    f_dstport = range2bdd(acl_rule['DstPort'], 16, 'dpt')
+
+    # src port
+    f_srcport = range2bdd(acl_rule['SrcPort'], 16, 'spt')
+
+    f = f_protocol & f_dstip & f_srcip & f_dstport & f_srcport
+    return f
+
+
+
+
+if __name__=="__main__":
+    #pred = ipp2bdd('70.4.193.0/24')
+    #print(bdd2expr(pred))
+    f = range2bdd('0-65535', 16, 'pro')
+    print(bdd2expr(f))
---
+++ main.py
@@ -0,0 +1,84 @@
+import yaml
+import os.path
+import ipaddress
+
+from bddutils import *
+
+ws_path = os.path.abspath(os.path.dirname(__file__))
+dp_path = os.path.join(ws_path, 'traces/dataplane/sample_dataplane.yml')
+with open(dp_path) as f:
+    dp = yaml.load(f, Loader=yaml.BaseLoader)
+
+if_dict = {} # dict for interface names
+for device in dp['Devices']: # build index for interface names
+    if_cnt = 0
+    for interface in device['Interfaces']:
+        if_dict[interface['Name']] = if_cnt
+        if_cnt = if_cnt + 1
+
+acl_dict = {} # dict for acl names
+for device in dp['Devices']: # build index for acl names
+    acl_cnt = 0
+    for acl in device['Acls']:
+        acl_dict[acl['Name']] = acl_cnt
+        acl_cnt = acl_cnt + 1
+
+
+def acl2pred(acl):
+    """Converts an ACL to a predicate.
+
+        Assume ACL first-match processing.
+    """
+    allowed = bddzeros(1)[0] # init with false
+    denied = bddzeros(1)[0]
+    
+    for rule in acl['Rules']:
+        if rule['Action'] == 'Deny':
+            denied = denied | (aclr2bdd(rule) & ~allowed)
+        else:
+            allowed = allowed | (aclr2bdd(rule) & ~denied)
+    if acl['DefaultAction'] == 'Deny':
+        return allowed
+    else:
+        return ~denied
+
+
+def rule_preflen(ft_rule): # helper function to sort forwarding table
+    ipn = ipaddress.IPv4Network(ft_rule['Prefix'])
+    return ipn.prefixlen
+
+def ft2preds(forwarding_table, interfaces):
+    """ Converts a forwarding table to predicates.
+
+        Returns:
+            A list of forwarding predicates for the device.
+            Each predicate correspondes to an interface.
+            Ordered by interface index values in if_dict.
+    """
+    preds = [bddzeros(1)[0] for interface in interfaces] # init with false
+
+    forwarding_table.sort(key=rule_preflen, reverse=True) # longest prefix first
+    
+    fwd = bddzeros(1)[0] # fwd <- false
+    for ft_rule in forwarding_table:
+        if_index = if_dict[ft_rule['Interface']]
+        prefix = ipp2bdd(ft_rule['Prefix']) # p <- p \/ (prefix /\ ~fwd)
+        preds[if_index] = preds[if_index] | (prefix & (~fwd))
+        fwd = fwd | prefix # fwd <- fwd \/ prefix
+    return preds
+
+
+   
+
+#preds = ft2preds(dp['Devices'][1]['ForwardingTable'], dp['Devices'][1]['Interfaces'])
+#print(dp['Devices'][1]['ForwardingTable'])
+#for pred in preds:
+#    print(bdd2expr(pred))
+pred = acl2pred(dp['Devices'][3]['Acls'][0])
+print(bdd2expr(pred))
+
+# acls = [acl2pred(device['Acls']) 
+#     for device in dp['Devices']]
+# fts = [ft2preds(device['ForwardingTable'], device['Interfaces']) 
+#     for device in dp['Devices']]
+
